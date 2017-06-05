f0c777f6864e2dd023a7cd742a9068233a039fc7
mattkohl <matt.kohl@thomsonreuters.com>
2017-06-05 11:59:51 +0100

    updated sense test

--- dictionary/api/tests.py
+++ dictionary/api/tests.py
@@ -1,15 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
 from api.models import Sense, Artist, Place, Song, Domain, SemanticClass, Annotation, Dictionary, Example
 
 
-class SenseAPITest(TestCase):
+class SenseAPITest(APITestCase):
 
     def setUp(self):
-        self.base_url = "/data/senses/"
-        self.instance_url = self.base_url + "{}/"
+        self.url = reverse('sense-list')
+        self.instance_url = self.url + "{}/"
         self.user = User.objects.create(username="test", email="ad@min.com", password="admin", is_superuser=True)
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
@@ -25,7 +27,7 @@ class SenseAPITest(TestCase):
             "holonyms": [],
             "annotations": [],
         }
-        request = self.client.post("/data/senses/", data, format='json')
-        self.assertEqual(request.status_code, 201)
-        sense_count = Sense.objects.all().count()
-        self.assertEqual(sense_count, 1)
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Sense.objects.count(), 1)
+        self.assertEqual(Sense.objects.get().headword, 'test sense')
