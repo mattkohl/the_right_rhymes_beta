d9e8d01b127a905c80679dba20c1a9f81d7df1a8
mattkohl <matt.kohl@thomsonreuters.com>
2017-06-06 08:52:42 +0100

    refactored test data

--- dictionary/api/tests/test_views.py
+++ dictionary/api/tests/test_views.py
@@ -19,41 +19,59 @@ class BaseApiTest(APITestCase):
         self.factory = RequestFactory()
 
 
+class DictionaryApiTest(BaseApiTest):
+
+    url = reverse('dictionary-list')
+    data = {
+        "name": "test dictionary",
+    }
+
+    def test_POST_dictionary(self):
+        self.data['owner'] = self.user.id
+        response = self.client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Dictionary.objects.count(), 1)
+        self.assertEqual(Dictionary.objects.get().name, 'test dictionary')
+
+
 class SenseApiTest(BaseApiTest):
 
     url = reverse('sense-list')
-    instance_url = url + "{}/"
+
+    data = {
+        "headword": "test sense",
+        "part_of_speech": "noun",
+        "derives_from": [],
+        "hyponyms": [],
+        "holonyms": [],
+        "annotations": [],
+    }
 
     def test_POST_sense(self):
-
-        data = {
-            "owner": self.user.id,
-            "headword": "test sense",
-            "part_of_speech": "noun",
-            "derives_from": [],
-            "hyponyms": [],
-            "holonyms": [],
-            "annotations": [],
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.data['owner'] = self.user.id
+        response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Sense.objects.count(), 1)
         self.assertEqual(Sense.objects.get().headword, 'test sense')
+
+    def test_GET_sense(self):
+        self.data['owner'] = self.user.id
+        self.client.post(self.url, self.data, format='json')
+        response = self.client.get(self.url, format="json")
+        self.assertEqual(response.data['count'], 1)
 
 
 class ArtistApiTest(BaseApiTest):
 
     url = reverse('artist-list')
-    instance_url = url + "{}/"
+    data = {
+        "name": "test artist",
+        "annotations": [],
+    }
 
     def test_POST_artist(self):
-
-        data = {
-            "owner": self.user.id,
-            "name": "test artist",
-            "annotations": [],
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.data['owner'] = self.user.id
+        response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Artist.objects.count(), 1)
         self.assertEqual(Artist.objects.get().name, 'test artist')
@@ -62,18 +80,16 @@ class ArtistApiTest(BaseApiTest):
 class PlaceApiTest(BaseApiTest):
 
     url = reverse('place-list')
-    instance_url = url + "{}/"
+    data = {
+        "full_name": "test city, test state, test country",
+        "annotations": [],
+        "within": [],
+        "artists": [],
+    }
 
     def test_POST_place(self):
-
-        data = {
-            "owner": self.user.id,
-            "full_name": "test city, test state, test country",
-            "annotations": [],
-            "within": [],
-            "artists": [],
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.data['owner'] = self.user.id
+        response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Place.objects.count(), 1)
         self.assertEqual(Place.objects.get().full_name, "test city, test state, test country")
@@ -84,34 +100,37 @@ class SongApiTest(BaseApiTest):
 
     url = reverse('song-list')
     artist_url = reverse('artist-list')
-    instance_url = url + "{}/"
+
+    artist_data = {
+        "name": "test artist",
+        "annotations": [],
+    }
+
+    song_data = {
+        "title": "test song",
+        "release_date_string": "2001",
+        "album": "test album",
+        "annotations": [],
+        "featured_artists": [],
+        "examples": []
+    }
 
     def test_POST_song(self):
-        request = self.factory.get('/data/')
-        host = request.get_host()
-        artist_data = {
-            "owner": self.user.id,
-            "name": "test artist",
-            "annotations": [],
-        }
-        response = self.client.post(self.artist_url, artist_data, format='json')
+
+        response = self.client.post(self.artist_url, self.artist_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Artist.objects.count(), 1)
         test_artist = Artist.objects.get()
+        request = self.factory.get('/data/')
+        host = request.get_host()
         test_artist_uri = make_uri(host, 'artists', test_artist.id)
+        self.song_data.update({"primary_artists": [test_artist_uri], "owner": self.user.id})
 
-        song_data = {
-            "owner": self.user.id,
-            "title": "test song",
-            "release_date_string": "2001",
-            "album": "test album",
-            "annotations": [],
-            "primary_artists": [test_artist_uri],
-            "featured_artists": [],
-            "examples": []
-        }
-        response = self.client.post(self.url, song_data, format='json')
+        self.song_data['owner'] = self.user.id
+        response = self.client.post(self.url, self.song_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Song.objects.count(), 1)
         self.assertEqual(Song.objects.get().title, "test song")
         self.assertEqual(Song.objects.get().release_date, date(2001, 12, 31))
+
+
