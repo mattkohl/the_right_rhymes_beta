c166842bbc355fea17777744530ccf2d91a56d63
mattkohl <hlaford@gmail.com>
2017-06-14 06:14:09 +0100

    added another permissions test

--- dictionary/api/tests/test_permissions.py
+++ dictionary/api/tests/test_permissions.py
@@ -3,16 +3,20 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
-from api.tests.test_models import BaseTest
+from api.tests.test_views import BaseApiTest
+from api.models import Sense
 
 
-class PermissionsTest(APITestCase):
+class UnauthenticatedPermissionsTest(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
 
     def test_unauthenticated_GET_all_senses(self):
 
         url = reverse('sense-list')
-        client = APIClient()
-        response = client.get(url)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_unauthenticated_POST_new_sense(self):
@@ -24,10 +28,42 @@ class PermissionsTest(APITestCase):
         }
 
         url = reverse('sense-list')
-        client = APIClient()
-        response = client.post(url, data, format="json")
+        response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertIn("Authentication credentials were not provided", str(response.content))
+
+
+class AuthenticatedPermissionsTest(BaseApiTest):
+
+    def test_authenticated_PUT_existing_sense(self):
+        data = {
+            "headword": "test sense",
+            "part_of_speech": "noun",
+            "definition": "test definition",
+            "derives_from": [],
+            "hyponyms": [],
+            "holonyms": [],
+            "annotations": [],
+        }
+
+        url = reverse('sense-list')
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        sense = Sense.objects.first()
+        self.assertEqual(sense.part_of_speech, "noun")
+
+        sense_url = response.data['url']
+        data['part_of_speech'] = "adverb"
+        response = self.client.put(sense_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        sense = Sense.objects.first()
+        self.assertEqual(sense.part_of_speech, "adverb")
+
+
+
+
+
 
 
 
