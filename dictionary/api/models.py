639c3fefbb5927cdc85ee8cba56646c87352c467
mattkohl <hlaford@gmail.com>
2017-06-16 07:33:43 +0100

    removed to_link

--- dictionary/api/models.py
+++ dictionary/api/models.py
@@ -364,12 +364,3 @@ class Annotation(models.Model):
             "place": self.place.to_xref(),
             "rhymes": [r.to_xref() for r in self.rhymes.all()]
         }
-
-    def to_link(self):
-        if self.sense:
-            return '<a href="{}">{}</a>'.format(self.sense.slug, self.text)
-        if self.artist:
-            return '<a href="{}">{}</a>'.format(self.artist.slug, self.text)
-        if self.place:
-            return '<a href="{}">{}</a>'.format(self.place.slug, self.text)
-        return '<a href="#">{}</a>'.format(self.text)
