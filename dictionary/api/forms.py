880333482e9042c4e7458a5fad7393931cd32de9
mattkohl <hlaford@gmail.com>
2017-06-29 06:27:42 +0100

    added more uniqueness constraints

--- dictionary/api/forms.py
+++ dictionary/api/forms.py
@@ -11,7 +11,18 @@ class AnnotationForm(forms.models.ModelForm):
     
     class Meta:
         model = Annotation
-        fields = ('text', 'example', 'sense', 'artist', 'place', 'rhymes',)
+        fields = ('text', 'example', 'sense', 'offset', 'artist', 'place', 'rhymes',)
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {
+                'text': [DUPLICATE_ERROR],
+                'offset': [DUPLICATE_ERROR],
+                'example': [DUPLICATE_ERROR]
+            }
+            self._update_errors(e)
 
 
 class ArtistForm(forms.models.ModelForm):
@@ -65,6 +76,13 @@ class ExampleForm(forms.models.ModelForm):
         model = Example
         fields = ('text', 'from_song',)
 
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ERROR], 'from_song': [DUPLICATE_ERROR]}
+            self._update_errors(e)
+
 
 class PlaceForm(forms.models.ModelForm):
 
@@ -80,7 +98,14 @@ class PlaceForm(forms.models.ModelForm):
         error_messages = {
             'full_name': {'required': EMPTY_NAME_ERROR}
         }
-        
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'full_name': [DUPLICATE_ERROR]}
+            self._update_errors(e)
+
         
 class SemanticClassForm(forms.models.ModelForm):
 
@@ -112,9 +137,31 @@ class SenseForm(forms.models.ModelForm):
                   'mentioned_in', 'derivatives', 'synonyms', 'antonyms', 'hypernyms',
                   'meronyms', 'domains', 'semantic_classes', 'dictionaries',)
 
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {
+                'definition': [DUPLICATE_ERROR],
+                'headword': [DUPLICATE_ERROR],
+                'part_of_speech': [DUPLICATE_ERROR]
+            }
+            self._update_errors(e)
+
 
 class SongForm(forms.models.ModelForm):
     class Meta:
         model = Song
         fields = ('title', 'primary_artists', 'featured_artists', 'release_date_string',
                   'album', 'lyrics', 'release_date_verified')
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {
+                'title': [DUPLICATE_ERROR],
+                'release_date_string': [DUPLICATE_ERROR],
+                'album': [DUPLICATE_ERROR]
+            }
+            self._update_errors(e)
